5cdba26025f43238aa511e26e0a1db7ff10eff8f
nehemiagueldi <gueldinehemia@gmail.com>
2025-01-08 09:47:53 +0700

    Back to First Commit

--- src/main/java/com/example/demo/repository/CVPersonRepository.java
+++ src/main/java/com/example/demo/repository/CVPersonRepository.java
@@ -1,13 +1,9 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.CVPerson;
 
-@Repository
 public interface CVPersonRepository extends JpaRepository<CVPerson, Integer> {
-    @Query(value = "SELECT COUNT(*) FROM tb_cv_person", nativeQuery=true)
-    public long countPerson();
+    // @Query("SELECT ")
 }
