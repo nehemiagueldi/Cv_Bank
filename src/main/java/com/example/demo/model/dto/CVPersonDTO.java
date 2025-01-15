5081b8208c1ec4b4792937868d00f7f5b0e088b9
AkriR <akrir24@gmail.com>
2025-01-15 17:04:58 +0700

    Rest Controller and DTO
    - Change endpoint get all cvperson by dto
    - Add atribut totalExperience and Age in CVPersonDTO

--- src/main/java/com/example/demo/model/dto/CVPersonDTO.java
+++ src/main/java/com/example/demo/model/dto/CVPersonDTO.java
@@ -1,5 +1,7 @@
 package com.example.demo.model.dto;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.example.demo.model.CVPerson;
@@ -18,11 +20,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CVPersonDTO {
+    private Integer totalExperience;
+    private Integer age;
     private CVPerson cvPerson;
-    private List<Project> projects;
-    private List<Education> educations;
-    private List<WorkExp> workExps;
-    private List<Training> trainings;
-    private List<CVTool> cvTools;
-    private List<CVSkill> cvSkills;
+    private List<Project> projects = new ArrayList<>();;
+    private List<Education> educations = new ArrayList<>();
+    private List<WorkExp> workExps = new ArrayList<>();
+    private List<Training> trainings = new ArrayList<>();
+    private List<CVTool> cvTools = new ArrayList<>();
+    private List<CVSkill> cvSkills = new ArrayList<>();
+
 }
