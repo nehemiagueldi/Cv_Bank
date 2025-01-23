f599c9fe74ddd6bf3111cd2bab4575afd4e28a9f
AkriR <akrir24@gmail.com>
2025-01-23 11:37:35 +0700

    DTO
    - Add Edit DTO for edit CV

--- src/main/java/com/example/demo/model/dto/CVPersonEditDTO.java
+++ src/main/java/com/example/demo/model/dto/CVPersonEditDTO.java
@@ -1,5 +1,12 @@
 package com.example.demo.model.dto;
 
+import java.util.List;
+
+import com.example.demo.model.Education;
+import com.example.demo.model.Project;
+import com.example.demo.model.Training;
+import com.example.demo.model.WorkExp;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,4 +18,10 @@ public class CVPersonEditDTO {
     private String name;
     private String position;
     private String summary;
+    private List<Integer> skillsSelected;
+    private List<Integer> toolsSelected;
+    private List<WorkExp> workExp;
+    private List<Project> projects;
+    private List<Training> trainings;
+    private List<Education> educations;
 }
